925a295cd20359acad67290e936ac6b710659f68
Jeremy French <jeremy@jeremyfrench.co.uk>
2023-04-15 09:48:58 +0000

    parse tabe from text area into data frame and output as table.

--- jssrc/stats.js
+++ jssrc/stats.js
@@ -1,3 +1,14 @@
-
 import { parse_csv } from "../pkg/index.js";
 import { dataframe_to_html_table } from "../pkg/index.js";
+
+
+let gobutton = document.getElementById('go');
+gobutton.addEventListener('click', () => { 
+let data = document.getElementById('csv').value;
+let resultsDiv = document.getElementById('results');
+const csv = parse_csv(data);
+resultsDiv.innerHTML = dataframe_to_html_table(csv);	
+});
+
+
+
